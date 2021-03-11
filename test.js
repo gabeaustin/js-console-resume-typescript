0c3a1386477df1b7f997dc2b607b4eeb231481e5
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-11 00:14:39 -0600

    added resume.ts to the tsconfig json file

--- test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var currentDate = new Date();
-var currentMonth = currentDate.getMonth();
-console.log("Current Date: " + currentDate + "\n Current Month: " + currentMonth);
--- test.ts
+++
@@ -1,4 +0,0 @@
-let currentDate = new Date();
-let currentMonth = currentDate.getMonth();
-
-console.log(`Current Date: ${currentDate}\n Current Month: ${currentMonth}`);
